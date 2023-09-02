2f2d9e2bea7e12578a5927a88d895438720e9c8a
Arad Sheybak <arad.sheybak@outlook.com>
2023-09-02 18:32:01 +0330

    getting user amount to betting added & readme updated

--- README.md
+++ README.md
@@ -13,4 +13,6 @@ step2-> in the terminal run npm init, The “npm init” command will initialize
 step3-> run npm i prompt-sync to getting user inputs with node
 
 
+in project.js you can find the all steps we must do in this project. the steps are sorted by what we do in sequence.
 
+prompt-sync help us to collect user inputs, as you can see we got the user amount to betting with prompt-sync i the first function.
---
+++ project.js
@@ -0,0 +1,16 @@
+// theory of the game
+// 1-> deposit some money for start game
+// 2-> determine number of lines to bet on
+// 3-> collect how many user is betting on this machine, collect a bet amount
+// 4-> Spin the slot machine
+// 5-> check it the user won
+// 6-> give the user their winning
+// 7-> play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () =>{
+    const depositAmount = prompt("Enter a amount you want to betting: ")
+};
+
+deposit();
