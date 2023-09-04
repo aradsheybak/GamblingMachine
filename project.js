380457798008e6bfa505008d8dd71ea7a5933665
Arad Sheybak <arad.sheybak@outlook.com>
2023-09-04 17:54:47 +0330

    check entered number for amount of betting and getting number of lines to bet added

--- project.js
+++ project.js
@@ -10,7 +10,30 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () =>{
-    const depositAmount = prompt("Enter a amount you want to betting: ")
+    const depositAmount = prompt("Enter a amount you want to betting: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    //check amount is not irrelevant string like 'hello' or is 0 or is negative 
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+        console.log("Invalid deposit amount try again");
+    }else{
+        return numberDepositAmount;
+    }
 };
 
-deposit();
+const getNumberOfLines = () =>{
+    while(true){
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <=0 || numberOfLines>3){
+            console.log("Invalid number of lines")
+        }else{
+            return numberOfLines
+        }
+    }
+}
+
+const depositAmpount = deposit();
+const numberOfLines = getNumberOfLines()
+console.log(numberOfLines)
